e3767b923c4d516458a6cfe5f4030d5ee9f0051f
yanotahck <yetanotherdingus@gmail.com>
2025-09-26 01:46:31 -0400

    single state select and multi state select, able to run scripts with threading

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# Once all states are written, another manager.py will be written and all functions will be called from the root thread in main.py. Before that this will be empty.
+import sys
+from states import stateScrapers, run_all
 
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        state = sys.argv[1].lower()
+        if state in stateScrapers:
+            stateScrapers[state]()
+        else:
+            print("Unknown state:", state)
+    else:
+        run_all()
